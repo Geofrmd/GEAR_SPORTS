f07e90a5ea376a1733adab42f48e097e754cc78f
estelle dupuy <estelledupuy@yahoo.fr>
2023-03-02 18:02:45 +0100

    WIP - booking source to fix + beautifying

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,6 @@ class Booking < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :start_date, presence: true
   validates :end_date, presence: true
+  STATUS = ["pending", "confirmed"]
+  validates :status, presence: true, inclusion: { in: STATUS }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_02_110341) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_144556) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_02_110341) do
     t.bigint "offer_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
     t.index ["offer_id"], name: "index_bookings_on_offer_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
